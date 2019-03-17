62fe45858715a152c5b96b1282510a0cb38a7495
Paul Anderson <paul.robert.anderson@gmail.com>
2019-03-18 08:56:42 +1100

    Improve Drive logic. Disk object used to store content

--- VzEmulator/Peripherals/Disk.cs
+++ VzEmulator/Peripherals/Disk.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VzEmulator.Peripherals
 {
-    class Disk
+    public class Disk
     {
+        public const int Size = 99160;
+        public const int TrackLength = 2480;   // 99160/40
+        private string fileName;
+        private byte[] contents;
+        public byte this[int address] {
+            get
+            {
+                return contents[address];
+            } 
+            set
+            {
+                contents[address] = value;
+            }
+        }
+        public int Length { get
+            {
+                return contents.Length;
+            }
+        }
+        
+        public Disk()
+        {
+            contents = new Byte[Size];
+        }
+
+        public void LoadDiskImage(string fileName)
+        {
+            contents = File.ReadAllBytes(fileName);
+            this.fileName = fileName;
+        }
+        public void SaveDiskImage(string fileName)
+        {
+            File.WriteAllBytes(fileName, contents);
+            this.fileName = fileName;
+        }
     }
 }
--- VzEmulator/Peripherals/Drive.cs
+++ VzEmulator/Peripherals/Drive.cs
@@ -9,13 +9,10 @@ namespace VzEmulator.Peripherals
 {
     public class Drive : IPeripheral
     {
-
+        const ushort DosInitRoutineAddress = 0x4b08;
         byte lastVal = 0;
-        byte[] diskContents;
-        int diskIndex;
         int diskCurrentTrack = 5;
         int diskLocationOntrack = 0;
-        const int diskTrackLength = 2480;   // 99160/40
         int lastStepNo;
         int lastStepNo2;
         int? prevFileIndex = null;
@@ -26,18 +23,19 @@ namespace VzEmulator.Peripherals
         byte currentWritingByte = 0;
         int bytesWritten = 0;
         bool inDosInitRoutine = false;
+        Disk diskContent = new Disk();
 
         public Tuple<ushort, ushort> PortRange => new Tuple<ushort, ushort>(0x10, 0x1f);
 
         public Tuple<ushort, ushort> MemoryRange => new Tuple<ushort, ushort>(0x4b08, 0x4b08);
 
-        private bool _debugEnabled = true ;
+        private bool _debugEnabled = false ;
         public bool DebugEnabled { get => _debugEnabled; set => _debugEnabled = value; }
 
         public byte? HandleMemoryRead(ushort address)
         {
-            //intercept dos init keyword, reset to start of file to align sectors
-            if (address == 0x4b08 )
+            //intercept dos init keyword, used to reset to start of file to align sectors
+            if (address == DosInitRoutineAddress )
             {
                 inDosInitRoutine = true;
             }
@@ -46,7 +44,7 @@ namespace VzEmulator.Peripherals
 
         public void HandleMemoryWrite(ushort address, byte value)
         {
-            
+            //Do nothing
         }
 
         public byte? HandlePortRead(ushort address)
@@ -55,24 +53,15 @@ namespace VzEmulator.Peripherals
 
             if (address == 0x11)
             {
-                if (diskContents == null)
-                {
-                    diskContents = File.ReadAllBytes("SavedFiles\\test1.dsk");
-                }
-
                 inDosInitRoutine = false;
 
-                if (diskIndex >= diskContents.Length)
-                    diskIndex = 0;
-
-                int fileIndex = (diskCurrentTrack * diskTrackLength) + (diskLocationOntrack / 8);
+                int fileIndex = (diskCurrentTrack * Disk.TrackLength) + (diskLocationOntrack / 8);
 
                 //bounds check fileIndex
                 if (fileIndex < 0) fileIndex = 0;
-                if (fileIndex > diskContents.Length - 1) fileIndex = diskContents.Length - 1;
+                if (fileIndex > diskContent.Length - 1) fileIndex = diskContent.Length - 1;
 
-                value = diskContents[fileIndex]; //Dos routine reads port 11 once for each change to bit 7 of port 12. 
-                diskIndex++; //Todo reset location on track seek (port 10). That will save scanning the whole disk
+                value = diskContent[fileIndex]; //Dos routine reads port 11 once for each change to bit 7 of port 12. 
 
                 if (!prevFileIndex.HasValue || prevFileIndex != fileIndex)
                 {
@@ -105,7 +94,7 @@ namespace VzEmulator.Peripherals
                     {
                         //update position unless in write mode (write function takes care of its own location)
                         diskLocationOntrack++;
-                        if (diskLocationOntrack > (diskTrackLength * 8) + 10)
+                        if (diskLocationOntrack > (Disk.TrackLength * 8) + 10)
                             diskLocationOntrack = 0;
                     }
                 }
@@ -123,134 +112,143 @@ namespace VzEmulator.Peripherals
 
         public void HandlePortWrite(ushort address, byte value)
         {
-            /*
-            Bit 0 - 3 : stepper motor
-            Bit 4 : Drive 1 Active (1=active)
-            Bit 5 : Write Data
-            Bit 6 : Write Request. Enable? (0=Active)
-            Bit 7 : Drive 2 Active (1=active)
-             */
             if (address == 0x10)
             {
                 //Disk drive control
-                if (diskContents == null)
-                {
-                    diskContents = File.ReadAllBytes("SavedFiles\\test1.dsk");
-                }
-
+                /*
+                Bit 0 - 3 : stepper motor
+                Bit 4 : Drive 1 Active (1=active)
+                Bit 5 : Write Data
+                Bit 6 : Write Request. Enable? (0=Active)
+                Bit 7 : Drive 2 Active (1=active)
+                 */
                 byte stepNo = (byte)(value & 0xF);
-                if ((stepNo == 1 && lastStepNo == 9 && lastStepNo2 == 8)
-                    | (stepNo == 4 && lastStepNo == 6 && lastStepNo2 == 2))
-                {
-                    diskCurrentTrack += 1;
-                    if (diskCurrentTrack > 40) diskCurrentTrack = 40;
-                    if (_debugEnabled) Console.WriteLine("Disk Track: {0}", diskCurrentTrack);
+                Step(stepNo);
 
-                }
-                if ((stepNo == 8 && lastStepNo == 9 && lastStepNo2 == 1) |
-                        (stepNo == 2 && lastStepNo == 6 && lastStepNo2 == 4))
-                {
-                    diskCurrentTrack -= 1;
-                    if (diskCurrentTrack < 0) diskCurrentTrack = 0;
-                    if (_debugEnabled) Console.WriteLine("Disk Track: {0}", diskCurrentTrack);
+                byte writeControl = (byte)(value & 0xF0);
+                Write(writeControl);
+                
+            }
+        }
 
-                }
-                lastStepNo2 = lastStepNo;
-                lastStepNo = stepNo;
-                prevFileIndex = null;
-                //end stepper motor logic
-
-                bool isWriteEnable = !((value & 0x40) == 0x40);
-                if (isWriteEnable)
+        private void Write(byte value)
+        {
+            /*
+                Bit 4 : Drive 1 Active(1 = active)
+                Bit 5 : Write Data
+                Bit 6 : Write Request. Enable ? (0 = Active)
+                Bit 7 : Drive 2 Active(1 = active)
+            */
+            bool isWriteEnable = !((value & 0x40) == 0x40);
+            if (isWriteEnable)
+            {
+                if (prevWriteEnable)
                 {
-                    if (prevWriteEnable)
+                    if ((writeBitNo & 1) == 0)
                     {
-                        if ((writeBitNo & 1) == 0)
-                        {
-                            //first written bit of actual bit. Just store it for later comparison
-                            firstHalfBit = (byte)((value & 0x20) >> 5);
-                        }
-                        else
-                        {
-                            byte thisHalfBit = (byte)((value & 0x20) >> 5);
-                            byte xorResult = (byte)(firstHalfBit ^ thisHalfBit);
-                            byte shiftedResult = (byte)(xorResult << 7 - (writeBitNo / 2));
-                            currentWritingByte |= shiftedResult;
-                        }
-                        if (writeBitNo == 15)
-                        {
-                            //all 8 bits received, write to file
-                            int fileIndex = (diskCurrentTrack * diskTrackLength) + (diskLocationOntrack / 8);
-
-                            //bounds check fileIndex
-                            if (fileIndex < 0) fileIndex = 0;
-                            if (fileIndex > diskContents.Length - 1) fileIndex = diskContents.Length - 1;
-
-                            //write byte. Todo: Write changes to file
-                            diskContents[fileIndex] = currentWritingByte;
-
-                            if (_debugEnabled)
-                            {
-                                if ((bytesWritten & 0x0F) == 0x0)
-                                {
-                                    Console.WriteLine();
-                                    Console.Write("Disk Write: {0:X4}: ", fileIndex);
-                                }
-                                Console.Write("{0:X2} ", currentWritingByte);
-                            }
-
-                            diskLocationOntrack += 8;
-                            bytesWritten++;
-                            writeBitNo = 0; //start next byte
-                            currentWritingByte = 0;
-                            firstHalfBit = 0;
-
-                        }
-                        else
-                        {
-                            writeBitNo++;
-                        }
+                        //first written bit of actual bit. Just store it for later comparison
+                        firstHalfBit = (byte)((value & 0x20) >> 5);
                     }
                     else
                     {
-                        //ignore first bit written, just result byte to be written
+                        byte thisHalfBit = (byte)((value & 0x20) >> 5);
+                        byte xorResult = (byte)(firstHalfBit ^ thisHalfBit);
+                        byte shiftedResult = (byte)(xorResult << 7 - (writeBitNo / 2));
+                        currentWritingByte |= shiftedResult;
+                    }
+                    if (writeBitNo == 15)
+                    {
+                        //all 8 bits received, write to file
+                        int fileIndex = (diskCurrentTrack * Disk.TrackLength) + (diskLocationOntrack / 8);
+
+                        //bounds check fileIndex
+                        if (fileIndex < 0) fileIndex = 0;
+                        if (fileIndex > diskContent.Length - 1) fileIndex = diskContent.Length - 1;
+
+                        //write byte
+                        diskContent[fileIndex] = currentWritingByte;
+
+                        if (_debugEnabled)
+                        {
+                            if ((bytesWritten & 0x0F) == 0x0)
+                            {
+                                Console.WriteLine();
+                                Console.Write("Disk Write: {0:X4}: ", fileIndex);
+                            }
+                            Console.Write("{0:X2} ", currentWritingByte);
+                        }
+
+                        diskLocationOntrack += 8;
+                        bytesWritten++;
+                        writeBitNo = 0; //start next byte
                         currentWritingByte = 0;
-                        writeBitNo = 0;
                         firstHalfBit = 0;
-
-                        //Adjust write location to next full byte
-                        if (inDosInitRoutine)
-                        {
-                            diskLocationOntrack = 0;
-                        }
-                        else
-                        {
-                            int writeOffset = 2; //How many bytes skipped in the time to switch from reading to writing
-                            diskLocationOntrack = ((int)(Math.Ceiling(diskLocationOntrack / 8.0) + writeOffset) * 8);
-                        }
-
-                        //Reset write counter
-                        bytesWritten = 0;
+                    }
+                    else
+                    {
+                        writeBitNo++;
                     }
                 }
                 else
                 {
-                    if (prevWriteEnable) Console.WriteLine();
+                    //ignore first bit written, just result byte to be written
+                    currentWritingByte = 0;
+                    writeBitNo = 0;
+                    firstHalfBit = 0;
+
+                    //Adjust write location to next full byte
+                    if (inDosInitRoutine)
+                    {
+                        diskLocationOntrack = 0;
+                    }
+                    else
+                    {
+                        int writeOffset = 2; //How many bytes skipped in the time to switch from reading to writing
+                        diskLocationOntrack = ((int)(Math.Ceiling(diskLocationOntrack / 8.0) + writeOffset) * 8);
+                    }
+
+                    //Reset write counter
+                    bytesWritten = 0;
                 }
-                prevWriteEnable = isWriteEnable;
+            }
+            else
+            {
+                if (prevWriteEnable) Console.WriteLine();
+            }
+            prevWriteEnable = isWriteEnable;
+
+        }
+
+        private void Step(byte stepNo)
+        {
+            if ((stepNo == 1 && lastStepNo == 9 && lastStepNo2 == 8)
+                                | (stepNo == 4 && lastStepNo == 6 && lastStepNo2 == 2))
+            {
+                diskCurrentTrack += 1;
+                if (diskCurrentTrack > 40) diskCurrentTrack = 40;
+                if (_debugEnabled) Console.WriteLine("Disk Track: {0}", diskCurrentTrack);
 
             }
+            if ((stepNo == 8 && lastStepNo == 9 && lastStepNo2 == 1) |
+                    (stepNo == 2 && lastStepNo == 6 && lastStepNo2 == 4))
+            {
+                diskCurrentTrack -= 1;
+                if (diskCurrentTrack < 0) diskCurrentTrack = 0;
+                if (_debugEnabled) Console.WriteLine("Disk Track: {0}", diskCurrentTrack);
+
+            }
+            lastStepNo2 = lastStepNo;
+            lastStepNo = stepNo;
+            //end stepper motor logic
         }
 
         public void LoadDiskImage(string fileName)
         {
-            diskContents = File.ReadAllBytes(fileName);
-
+            diskContent.LoadDiskImage(fileName);
         }
         public void SaveDiskImage(string fileName)
         {
-            File.WriteAllBytes(fileName, diskContents);
-
+            diskContent.SaveDiskImage(fileName);
         }
     }
 }
